a120413735d1fb6185f92828ec1f4640dddbd5ae
chaiopi932-stack <chaiopi932@gmail.com>
2026-06-16 04:03:45 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+const bgm = document.getElementById("bgm");
+const click = document.getElementById("click");
+
+bgm.src = "https://cdn.pixabay.com/download/audio/2022/03/15/audio_5c1b5d0d5d.mp3";
+
+document.body.addEventListener("click", () => {
+  bgm.play();
+}, { once: true });
+
+// 打字效果
+const text = "Welcome to Midgard...";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text[i];
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+typing();
+
+// 登入
+function login() {
+  alert("Entering Midgard...");
+}
