04b0171fcbb5013e32fa608847b27b4ad191e7f8
chaiopi932-stack <chaiopi932@gmail.com>
2026-06-16 05:56:42 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,8 @@
 console.log("JS LOADED");
 
-const text = "Welcome to Midgard...";
-let i = 0;
-
-function typing() {
-  if (i < text.length) {
-    document.getElementById("typing").innerHTML += text[i];
-    i++;
-    setTimeout(typing, 50);
-  }
-}
-typing();
-
 function login() {
   const win = document.querySelector(".window");
+
   win.style.transition = "0.5s";
   win.style.opacity = "0";
 
