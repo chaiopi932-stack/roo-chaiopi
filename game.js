6b6f6d054ee668dff13a00d6661b2ae529df72e2
chaiopi932-stack <chaiopi932@gmail.com>
2026-06-16 05:53:05 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,5 @@
-console.log("GAME JS LOADED");
-const bgm = document.getElementById("bgm");
-const click = document.getElementById("click");
+console.log("JS LOADED");
 
-bgm.src = "https://cdn.pixabay.com/download/audio/2022/03/15/audio_5c1b5d0d5d.mp3";
-
-document.body.addEventListener("click", () => {
-  bgm.play();
-}, { once: true });
-
-// 打字效果
 const text = "Welcome to Midgard...";
 let i = 0;
 
@@ -21,17 +12,12 @@ function typing() {
 }
 typing();
 
-// 登入
 function login() {
   const win = document.querySelector(".window");
-
-  // fade out
-  win.style.transition = "0.6s ease";
+  win.style.transition = "0.5s";
   win.style.opacity = "0";
-  win.style.transform = "translate(-50%, -50%) scale(0.9)";
 
   setTimeout(() => {
     win.style.display = "none";
-    console.log("Entered Midgard");
-  }, 600);
+  }, 500);
 }
